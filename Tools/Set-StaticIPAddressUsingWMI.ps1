9d51343cd144ad6d50cae3a7a40099fe220b9f8f
Jan Marek <jan.marek@technet.ms>
2019-03-25 19:35:44 +0100

    Create Set-StaticIPAddressUsingWMI.ps1

---
+++ Tools/Set-StaticIPAddressUsingWMI.ps1
@@ -0,0 +1,8 @@
+﻿function Set-StaticIPAddressUsingWMI {
+    param($IPAddress,$SubnetMask,$Gateway,$DNSServer)
+
+    $wmi_object = Get-WmiObject win32_networkadapterconfiguration -filter “ipenabled = ‘true'”
+    $wmi_object.EnableStatic($IPAddress, $SubnetMask)
+    $wmi_object.SetGateways($Gateway, 1)
+    $wmi_object.SetDNSServerSearchOrder($DNSServer)
+}
